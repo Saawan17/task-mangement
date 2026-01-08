ca3360aaefea3aa80f5be85be11fe96e14bbb106
Saawan k <saawank17@gmail.com>
2026-01-08 16:57:30 +0530

    adding annotations for repository and resolving the error in the repository

--- src/main/java/com/world/task/repository/TaskRepository.java
+++ src/main/java/com/world/task/repository/TaskRepository.java
@@ -4,12 +4,14 @@ package com.world.task.repository;
 import com.world.task.enums.TaskStatus;
 import com.world.task.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+@Repository
+public interface TaskRepository extends JpaRepository<TaskModel, String> {
 
     List<TaskModel> findByStatus(TaskStatus status);
 
-    List<TaskModel> findByAssignedTo_Id(Long userId);
+    List<TaskModel> findByAssignedTo_UserId(String userId);
 }
--- src/main/java/com/world/task/repository/UserRepository.java
+++ src/main/java/com/world/task/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.world.task.repository;
 
 import com.world.task.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserModel, String> {
 
     Optional<UserModel> findByEmail(String email);
